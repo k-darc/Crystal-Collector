af47ffa18fa490afe853e13ad78a4594923645b6
Kevin Darcy <neoglitch822@yahoo.com>
2019-09-30 23:51:22 -0400

    1

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+
+/*
+var generateRandomNum = Math.floor(Math.random() * 100) + 1;
+var winTotal =;
+var lossTotal =;
+var scoreTotal =;*/
+/*
+function getNumber() {
+    var minNumber = 0; // The minimum number you want
+    var maxNumber = 100; // The maximum number you want
+    var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // Generates random number
+    $('#random-number').html(randomnumber); // Sets content of <div> to number
+    return false; // Returns false just to tidy everything up
+
+    function myFunction() {
+        document.getElementById("random-number").innerHTML = Math.random();
+      }
