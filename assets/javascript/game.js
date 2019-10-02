026d0c1357d05ad7f4471b71407978e19ed1722e
Kevin Darcy <neoglitch822@yahoo.com>
2019-10-02 12:25:33 -0400

    finished the game... FINALLY. THank you Varun

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 
   var winTotal = 0
   var lossTotal = 0;
-
   var targetTotal = getRandomInt(19, 120);
   var counter = 0;
 
@@ -25,19 +24,42 @@
 
   /*$("#random-number").text(targetTotal); ... don't, just don't do this */
 
-  var numberOptions = [10, 5, 3, 7]; /*these HAVE to be random 3-10 */
-
+  var numberOptions = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]; /*these HAVE to be random 3-10 */
+/*
   for (var i = 0; i < numberOptions.length; i++) {
     var imageCrystal = $("<img>");
 
+    Math.floor(Math.random() * numberOptions.length);
+
     imageCrystal.addClass("crystal-image");
 
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    imageCrystal.attr("src", "assets/images/img-cry1.png"); 
 
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 
     $("#crystal-array").append(imageCrystal);
+
+    chooseRandom(imageCrystal, numberOptions[i]);
+  }*/
+
+/* -------------------------- random func  ---------*/
+chooseRando("assets/images/img-cry1.png");
+chooseRando("assets/images/img-cry2.png");
+chooseRando("assets/images/img-cry3.png");
+chooseRando("assets/images/img-cry4.png");
+
+  function chooseRando (imageLocation) {
+    var imageCrystal = $("<img>");
+
+    imageCrystal.addClass("crystal-image");
+
+    imageCrystal.attr("src", imageLocation); 
+
+    imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * numberOptions.length));
+
+    $("#crystal-array").append(imageCrystal);
   }
+  /*---------------------------*/
 
   $(".crystal-image").on("click", function() {
 
@@ -50,7 +72,7 @@
     $("#revolve").html(counter);
 
     if (counter === targetTotal) { 
-      document.getElementById('sound-win').play();
+      document.getElementById('sound-win').play(); /* counter doesn't display the winning or losing number before failing*/
       alert("YOU WIN !!");
       console.log("WIN");
       $("#revolve").html("");
