79bba39d2e7e6311ebcb13135b00d0efecdf874f
Kevin Darcy <neoglitch822@yahoo.com>
2019-10-01 21:40:54 -0400

    This is the 'after tutoring' screenshot- notice the improvements!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,14 +10,16 @@
       var minNumber = 19; // The minimum number you want
       var maxNumber = 121; // The maximum number you want
 
-      var randomnumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+      var randomnumber = (Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber);
       $('#random-number').html(randomnumber); // Sets content of <div> to number
+      $("#winC").html(winTotal);
+      $("#lossC").html(lossTotal);
+      console.log(randomnumber);
       return false; // Returns false just to tidy everything up
-
   }
   getNumber();
 
-  $("#random-number").text(targetTotal);
+  /*$("#random-number").text(targetTotal);*/
 
   var numberOptions = [10, 5, 3, 7];
 
@@ -30,25 +32,34 @@
 
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 
-    $(".revolving-total").append(imageCrystal);
+    $("#crystal-array").append(imageCrystal);
   }
 
   $(".crystal-image").on("click", function() {
 
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    
+
     counter += crystalValue;
-    console.log("Your total is " + counter);
+
+    console.log("Your total is " + counter); /*add this to "your total score is" which is the id .revolve*/
+    $("#revolve").html(counter);
 
     if (counter === targetTotal) {
-      console.log("YOU WIN");
+      alert("YOU WIN");
+      counter = 0;
+      winTotal++;
+      getNumber();
     }
     else if (counter >= targetTotal) {
-      console.log("You lose !");
-    }
-  });
+      alert("You lose !");
+      counter = 0;
+      lossTotal++;
+      getNumber();
 
+    }
+  }); /*clear revolving total at the end of the game*/
+/* crystal values have to become random*/
 
 
 
